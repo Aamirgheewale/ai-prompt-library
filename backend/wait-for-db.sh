4a6bb690e51dedf4c5882090466f5a59616aea54
aamir gheewale <aamirgheewale142210@gmail.com>
2026-04-16 18:10:51 +0530

    feat(backend): dockerize Django app with DB wait script and dependencies

---
+++ backend/wait-for-db.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "⏳ Waiting for PostgreSQL..."
+
+while ! nc -z postgres 5432; do
+  sleep 1
+done
+
+echo "✅ PostgreSQL is ready"
+
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
