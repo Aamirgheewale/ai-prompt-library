a54376f6da4c16adbba767732d4dad1873f354ec
aamir gheewale <aamirgheewale142210@gmail.com>
2026-04-17 10:24:14 +0530

    chore(backend): prepare Django for production deployment on Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9b&p05jhi$dzzevfj6n)u-pon+q2r)8*wmhacrdf9jp9azowq_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -144,3 +144,7 @@ LOGGING = {
         'level': 'WARNING',
     },
 }
+import os
+
+DEBUG = False
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
